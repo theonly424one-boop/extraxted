174340e5308c76cd0b060cd2bc3dd7ca98dace5e
theonly424one-boop <theonly424one@gmail.com>
2026-07-29 00:30:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8630903419:AAHA9pXG6bKBMBwgY9Ee9O4UckpuJoz1Rao")
+    API_ID = int(os.environ.get("API_ID", "8630903419"))
+    API_HASH = os.environ.get("API_HASH", "AAHA9pXG6bKBMBwgY9Ee9O4UckpuJoz1Rao")
     AUTH_USERS = "1411895712"
 
